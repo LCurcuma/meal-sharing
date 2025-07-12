72301415db62806bcfb22d99b8902adab44eb0bb
LCurcuma <zhihnadya2002@gmail.com>
2025-07-12 15:38:04 +0200

    added some files (struggling with render)

---
+++ api/knexfile.js
@@ -0,0 +1,17 @@
+export default {
+  client: process.env.DB_CLIENT || 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    ssl: process.env.DB_USE_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  },
+  migrations: {
+    directory: './migrations'
+  },
+  seeds: {
+    directory: './seeds'
+  }
+};
